c5d5f8b594288404865712b2092ad7128ec2a6b6
Yago Marinho <marinho.rj2@hotmail.com>
2026-01-10 10:47:17 -0300

    code header added

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,7 +1,14 @@
+/*
+ * Copyright (c) 2025 Yago Marinho (Davna)
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 module.exports = {
   singleQuote: true,
   semi: false,
-  arrowParens: "avoid",
-  trailingComma: "all",
+  arrowParens: 'avoid',
+  trailingComma: 'all',
   printWidth: 80,
-};
+}
